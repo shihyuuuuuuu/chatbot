951306422914424c19a59ef5158840ab409f8c4e
F74046187 <www.elmo20816@gmail.com>
2018-01-03 22:17:54 +0800

    Implement 2 functions: Game schedule & Team place

--- app.py
+++ app.py
@@ -17,36 +17,122 @@ machine = TocMachine(
         'user',
         'state1',
         'state2',
-		'state3'
+		'state3',
+		'state4',
+        'state5',
+        'state6',
+        'state7',
+        'state8',
+        'state9',
+        'state10'
     ],
     transitions=[
-        {
+        {#user to state1
             'trigger': 'advance',
             'source': 'user',
             'dest': 'state1',
             'conditions': 'is_going_to_state1'
         },
-        {
+        {#state1 to state2
             'trigger': 'advance',
             'source': 'state1',
             'dest': 'state2',
             'conditions': 'is_going_to_state2'
         },
-        {
+        {#state1 to state10
+            'trigger': 'advance',
+            'source': 'state1',
+            'dest': 'state10',
+            'conditions': 'is_going_to_state10'
+        },
+        {#state2 to state1
+            'trigger': 'advance',
+            'source': 'state2',
+            'dest': 'state1',
+            'conditions': 'is_going_to_state1'
+        },
+        {#state2 to state3
             'trigger': 'advance',
             'source': 'state2',
             'dest': 'state3',
             'conditions': 'is_going_to_state3'
         },
+        {#state2 to state4
+            'trigger': 'advance',
+            'source': 'state2',
+            'dest': 'state4',
+            'conditions': 'is_going_to_state4'
+        },
+        {#state2 to state5
+            'trigger': 'advance',
+            'source': 'state2',
+            'dest': 'state5',
+            'conditions': 'is_going_to_state5'
+        },
+        {#state2 to state6
+            'trigger': 'advance',
+            'source': 'state2',
+            'dest': 'state6',
+            'conditions': 'is_going_to_state6'
+        },
+        {#state2 to state7
+            'trigger': 'advance',
+            'source': 'state2',
+            'dest': 'state7',
+            'conditions': 'is_going_to_state7'
+        },
+        {#state2 to state8
+            'trigger': 'advance',
+            'source': 'state2',
+            'dest': 'state8',
+            'conditions': 'is_going_to_state8'
+        },
+        {#state2 to state9
+            'trigger': 'advance',
+            'source': 'state2',
+            'dest': 'state9',
+            'conditions': 'is_going_to_state9'
+        },
+        {#state2 to user
+            'trigger': 'advance',
+            'source': 'state2',
+            'dest': 'user',
+            'conditions': 'bye_to_user'
+        },
+        {#state1 to user
+            'trigger': 'advance',
+            'source': 'state1',
+            'dest': 'user',
+            'conditions': 'bye_to_user'
+        },
         {
             'trigger': 'go_back',
             'source': [
-                'state1',
                 'state2',
-				'state3'
             ],
-            'dest': 'user'
+            'dest': 'state1'
+        },
+        {
+            'trigger': 'go_back',
+            'source': [
+                'state10',
+            ],
+            'dest': 'state1'
+        },
+        {
+            'trigger': 'go_back',
+            'source': [
+                'state3',
+                'state4',
+                'state5',
+                'state6',
+                'state7',
+                'state8',
+                'state9',
+            ],
+            'dest': 'state2'
         }
+        
     ],
     initial='user',
     auto_transitions=False,
--- fsm.py
+++ fsm.py
@@ -14,32 +14,72 @@ class TocMachine(GraphMachine):
             **machine_configs
         )
 
+    def bye_to_user(self, update):
+        text = update.message.text
+        if text.lower() == 'bye':
+            update.message.reply_text("ㄅㄅ")
+            return 1
+
     def is_going_to_state1(self, update):
         text = update.message.text
-        return text.lower() == 'hi'
+        return text.lower() == '嗨' or text.lower() == '返回'
 
     def is_going_to_state2(self, update):
         text = update.message.text
-        return text.lower() == 'schedule'
+        if text.lower() == '查賽程':
+            return 1
 
     def is_going_to_state3(self, update):
         text = update.message.text
-        if text.lower() == 'yu':
+        if text.lower() == '裕隆':
             return 1
 
+    def is_going_to_state4(self, update):
+        text = update.message.text
+        if text.lower() == '台啤':
+            return 1
+    def is_going_to_state5(self, update):
+        text = update.message.text
+        if text.lower() == '璞園':
+            return 1
+
+    def is_going_to_state6(self, update):
+        text = update.message.text
+        if text.lower() == '達欣':
+            return 1
+    def is_going_to_state7(self, update):
+        text = update.message.text
+        if text.lower() == '富邦':
+            return 1
+
+    def is_going_to_state8(self, update):
+        text = update.message.text
+        if text.lower() == '台銀':
+            return 1
+    def is_going_to_state9(self, update):
+        text = update.message.text
+        if text.lower() == '金酒':
+            return 1
+
+    def is_going_to_state10(self, update):
+        text = update.message.text
+        if text.lower() == '查戰績':
+            return 1
+    
     def on_enter_state1(self, update):
-        update.message.reply_text("Bello!我可以為你做什麼呢？")
+        update.message.reply_text("你好!我可以為你做什麼呢？")
 
     def on_exit_state1(self, update):
         print('Leaving state1')
 
     def on_enter_state2(self, update):
-        update.message.reply_text("你想要查那一隊呢～？")
-
+        update.message.reply_text("你想要看那一隊的比賽？")
+    
     def on_exit_state2(self, update):
         print('Leaving state2')
     
     def on_enter_state3(self, update):
+        update.message.reply_text("這是裕隆隊接下來的比賽：")
         for document in schedule.find({"$or": [{"Team1":"裕隆"}, {"Team2":"裕隆"}]}):
             document = str(document)
             date = document.find("Date")
@@ -48,8 +88,108 @@ class TocMachine(GraphMachine):
             team2 = document.find("Team2")
             place = document.find("Place")
 			#print(document[date+8:date+13], document[time+6, time+11])
-            update.message.reply_text("這是裕隆隊接下來的比賽：\n"+document[date+8:date+13] + " " + document[time+8:time+13] + " " + document[team1+9:team1+11] + ":" + document[team2+9:team2+11] + " " + document[place+9:place+11])
+            update.message.reply_text(document[date+8:date+14] + " " + document[time+8:time+13] + " " + document[team1+9:team1+11] + ":" + document[team2+9:team2+11] + " " + document[place+9:place+11])
         self.go_back(update)
 
     def on_exit_state3(self, update):
         print('Leaving state3')
+    
+    def on_enter_state4(self, update):
+        update.message.reply_text("這是台啤隊接下來的比賽：")
+        for document in schedule.find({"$or": [{"Team1":"台啤"}, {"Team2":"台啤"}]}):
+            document = str(document)
+            date = document.find("Date")
+            time = document.find("Time")
+            team1 = document.find("Team1")
+            team2 = document.find("Team2")
+            place = document.find("Place")
+            update.message.reply_text(document[date+8:date+14] + " " + document[time+8:time+13] + " " + document[team1+9:team1+11] + ":" + document[team2+9:team2+11] + " " + document[place+9:place+11])
+        self.go_back(update)
+
+    def on_exit_state4(self, update):
+        print('Leaving state4')
+    
+    def on_enter_state5(self, update):
+        update.message.reply_text("這是璞園隊接下來的比賽：")
+        for document in schedule.find({"$or": [{"Team1":"璞園"}, {"Team2":"璞園"}]}):
+            document = str(document)
+            date = document.find("Date")
+            time = document.find("Time")
+            team1 = document.find("Team1")
+            team2 = document.find("Team2")
+            place = document.find("Place")
+            #print(document[date+8:date+13], document[time+6, time+11])
+            update.message.reply_text(document[date+8:date+14] + " " + document[time+8:time+13] + " " + document[team1+9:team1+11] + ":" + document[team2+9:team2+11] + " " + document[place+9:place+11])
+        self.go_back(update)
+
+    def on_exit_state5(self, update):
+        print('Leaving state5')
+    
+    def on_enter_state6(self, update):
+        update.message.reply_text("這是達欣隊接下來的比賽：")
+        for document in schedule.find({"$or": [{"Team1":"達欣"}, {"Team2":"達欣"}]}):
+            document = str(document)
+            date = document.find("Date")
+            time = document.find("Time")
+            team1 = document.find("Team1")
+            team2 = document.find("Team2")
+            place = document.find("Place")
+            update.message.reply_text(document[date+8:date+14] + " " + document[time+8:time+13] + " " + document[team1+9:team1+11] + ":" + document[team2+9:team2+11] + " " + document[place+9:place+11])
+        self.go_back(update)
+
+    def on_exit_state6(self, update):
+        print('Leaving state6')
+    
+    def on_enter_state7(self, update):
+        update.message.reply_text("這是富邦隊接下來的比賽：")
+        for document in schedule.find({"$or": [{"Team1":"富邦"}, {"Team2":"富邦"}]}):
+            document = str(document)
+            date = document.find("Date")
+            time = document.find("Time")
+            team1 = document.find("Team1")
+            team2 = document.find("Team2")
+            place = document.find("Place")
+            #print(document[date+8:date+13], document[time+6, time+11])
+            update.message.reply_text(document[date+8:date+14] + " " + document[time+8:time+13] + " " + document[team1+9:team1+11] + ":" + document[team2+9:team2+11] + " " + document[place+9:place+11])
+        self.go_back(update)
+
+    def on_exit_state7(self, update):
+        print('Leaving state7')
+    
+    def on_enter_state8(self, update):
+        update.message.reply_text("這是台銀隊接下來的比賽：")
+        for document in schedule.find({"$or": [{"Team1":"台銀"}, {"Team2":"台銀"}]}):
+            document = str(document)
+            date = document.find("Date")
+            time = document.find("Time")
+            team1 = document.find("Team1")
+            team2 = document.find("Team2")
+            place = document.find("Place")
+            update.message.reply_text(document[date+8:date+14] + " " + document[time+8:time+13] + " " + document[team1+9:team1+11] + ":" + document[team2+9:team2+11] + " " + document[place+9:place+11])
+        self.go_back(update)
+
+    def on_exit_state8(self, update):
+        print('Leaving state8')
+
+    def on_enter_state9(self, update):
+        update.message.reply_text("這是金酒隊接下來的比賽：")
+        for document in schedule.find({"$or": [{"Team1":"金酒"}, {"Team2":"金酒"}]}):
+            document = str(document)
+            date = document.find("Date")
+            time = document.find("Time")
+            team1 = document.find("Team1")
+            team2 = document.find("Team2")
+            place = document.find("Place")
+            #print(document[date+8:date+13], document[time+6, time+11])
+            update.message.reply_text(document[date+8:date+14] + " " + document[time+8:time+13] + " " + document[team1+9:team1+11] + ":" + document[team2+9:team2+11] + " " + document[place+9:place+11])
+        self.go_back(update)
+
+    def on_exit_state9(self, update):
+        print('Leaving state9')
+
+    def on_enter_state10(self, update):
+        update.message.reply_text("1 台灣銀行\n2 桃園璞園建築\n3 富邦勇士\n4 台北達欣工程\n5 金門酒廠\n6 裕隆納智捷\n7 台灣啤酒")
+        self.go_back(update)
+    
+    def on_exit_state10(self, update):
+        print('Leaving state10')
